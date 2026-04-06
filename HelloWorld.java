b429508bcef7a7ed94af20dd56eb45178b185fb7
kp7135 <kp7135@srmist.edu.in>
2026-04-06 14:58:32 +0530

    UC5: Enhanced for loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,17 +2,10 @@
     public static void main(String[] args) {
 
         if (args.length == 0) {
-            System.out.println("Hello World");
+            System.out.println("Hello, World!");
         } else {
-            System.out.print("Hello ");
-
-            for (int i = 0; i < args.length; i++) {
-                System.out.print(args[i]);
-
-                // Add comma between names
-                if (i < args.length - 1) {
-                    System.out.print(", ");
-                }
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
             }
         }
     }
