0ac0310bb4c0c5461d5219cb97c7ae8d2da07980
kp7135 <kp7135@srmist.edu.in>
2026-04-06 15:06:08 +0530

    UC6: Used substring to remove trailing delimiter

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,21 @@
- public class HelloWorld {
+public class HelloWorld {
     public static void main(String[] args) {
 
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            for (String name : args) {
-                System.out.println("Hello, " + name + "!");
+
+            String result = "Hello, ";
+
+                       for (String name : args) {
+                result += name + ", ";
             }
+
+                       result = result.substring(0, result.length() - 2);
+
+            result += "!";
+
+            System.out.println(result);
         }
     }
 }
